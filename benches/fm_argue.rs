bda19529e43b94873d7ee96d3b2de4fba38d525b
Josh Stoik <josh@blobfolio.com>
2022-05-28 21:05:14 -0700

    misc: bench option, switch2

--- benches/fm_argue.rs
+++ benches/fm_argue.rs
@@ -12,8 +12,30 @@ use brunch::{
 use argyle::Argue;
 use std::time::Duration;
 
+fn argument() -> Argue {
+	[
+		&b"-k"[..],
+		b"--key=val",
+		b"-x",
+		b"out",
+		b"--quiet",
+		b"/foo/bar",
+		b"/bar/baz",
+	].into_iter().collect()
+}
+
 benches!(
 	Bench::new("argyle::Argue", "new(0)")
 		.timed(Duration::from_secs(1))
 		.with(|| Argue::new(0)),
+
+	Bench::spacer(),
+
+	Bench::new("argyle::Argue", "option(-x)")
+		.timed(Duration::from_secs(1))
+		.with_setup_ref(argument(), |a| a.option(b"-x").is_some()),
+
+	Bench::new("argyle::Argue", "switch2(-q, --quiet)")
+		.timed(Duration::from_secs(1))
+		.with_setup_ref(argument(), |a| a.switch2(b"-q", b"--quiet")),
 );
